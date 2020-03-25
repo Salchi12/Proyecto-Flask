00eaf8eb6bd40bc6b8a6aa3a79794911dec86f5d
Salchi12 <jparedesb@unitru.edu.pe>
2020-03-24 23:18:37 -0500

    modificacion conncetion db y registro

--- app.py
+++ app.py
@@ -1,65 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template,request,redirect,url_for,flash
 from flaskext.mysql import MySQL
+import app
+
 
 app = Flask(__name__)
 
-
-#MySQL config
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'contactos'
+#MYSQL CONNECTION
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='contactos'
 mysql = MySQL()
 mysql.init_app(app)
 
-#Settings
+#SETTINGS
+app.secret_key ='mysecretkey'
 
-#app.secret_key = 'mysecretkey'
-
-@app.route('/')
+@app.route("/")
 def login():
-	cur = mysql.get_db().cursor()
-	cur.execute('SELECT email FROM contacs')
-	data = cur.fetchall()
-	print(data)
-	return render_template('login.html')
-
-@app.route('/register')
+    return render_template('login.html')
+@app.route("/registro")
 def register():
-	return render_template('register.html')
+    return render_template('register.html')
+@app.route("/add_contact", methods=['POST'])
+def add_contact():
 
-@app.route('/datos', methods=['POST'])
-def datos():
-	if request.method =='POST':
-		fullname = request.form['fullname']
-		phone = request.form['phone']
-		email = request.form['email']
-		password = request.form['password']
-		cur = mysql.connection.cursor()
-		cur.execute('INSERT INTO contacs (fullname, phone, email, password) VALUES (%s, %s, %s, %s)', 
-			(fullname, phone, email, password))
-		mysql.connection.commit()
-		flash('CONTACTO CREADO')
-		return redirect(url_for('login'))
+    if request.method == 'POST':
+        name = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cursor = mysql.get_db().cursor()
+        cursor.execute(' INSERT INTO contacs (nombre, telefono, email) VALUES (%s, %s, %s)', (name, phone, email))
+        print(name)
+        flash('Se guardo el contacto satisfactoriamente')
+        return redirect(url_for('login'))
 
-
-@app.route('/validar', methods=['POST'])
-def validar():
-	if request.method == 'POST':
-		usuario = request.form['user']
-		contraseña = request.form['password']
-	if (usuario == "salchi") and (contraseña == "123456"):
-		return home()
-	else:
-		return login()
-
-@app.route('/home')
-def home():
-	return render_template('home.html')
-	
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-if __name__ == '__main__':
-	app.run(port = 5000,debug=True)
+if __name__=='__main__':
+    app.run(port=3000, debug=True)
